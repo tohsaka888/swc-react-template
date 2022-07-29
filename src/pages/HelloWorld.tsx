8fe669ebe4a9d6f02449f3347fb387dfadf98a45
tohsaka888 <156132264@qq.com>
2022-07-29 10:57:23 +0800

    ✨ feat: 添加模板页面

---
+++ src/pages/HelloWorld.tsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-07-29 10:34:32
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-07-29 10:38:01
+ * @Description: 请填写简介
+ */
+
+import React from 'react'
+import Container from '@/components/HelloWorld/Container'
+
+function HelloWorld() {
+  return (
+    <Container />
+  )
+}
+
+export default HelloWorld
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,18 +2,16 @@
  * @Author: tohsaka888
  * @Date: 2022-07-28 16:09:12
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-07-28 16:20:09
+ * @LastEditTime: 2022-07-29 10:41:00
  * @Description: 请填写简介
  */
 
 import React from 'react'
-import Container from '../components/HomePage/Container'
+import Container from '@/components/HomePage/Container'
 
 function HomePage() {
   return (
-    <div>
-      <Container />
-    </div>
+    <Container />
   )
 }
 
