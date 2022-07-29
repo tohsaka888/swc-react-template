1da9f281ccc8c4174fee13f177a7246a6d7e92c6
tohsaka888 <156132264@qq.com>
2022-07-29 10:56:45 +0800

    ✨ feat: HomePage

--- src/components/HomePage/Container.tsx
+++ src/components/HomePage/Container.tsx
@@ -2,22 +2,24 @@
  * @Author: tohsaka888
  * @Date: 2022-07-28 16:09:46
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-07-28 17:03:25
+ * @LastEditTime: 2022-07-29 10:53:38
  * @Description: 请填写简介
  */
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 import style from './index.module.css'
 
 function Container() {
   return (
     <div className={style['container']}>
-      <div className={style['main-title']}>
+      <div className={`${style['main-title']} ${style['mb-8']}`}>
         HomePage
       </div>
-      <div className={style['sub-title']}>
+      <div className={`${style['sub-title']} ${style['mb-8']}`}>
         Welcome!
       </div>
+      <Link to={'/hello-world'}>Go To HelloWorld</Link>
     </div>
   )
 }
