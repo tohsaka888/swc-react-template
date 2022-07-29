522aacab8eb3289d57d2dcc690dd44eab295f256
tohsaka888 <156132264@qq.com>
2022-07-29 10:55:59 +0800

    ✨ feat: 配置路由

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@
  * @Author: tohsaka888
  * @Date: 2022-07-28 11:04:15
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-07-28 16:20:20
- * @Description: 请填写简介
+ * @LastEditTime: 2022-07-29 10:55:44
+ * @Description: 路由配置
  */
 
 import React from 'react'
 import '@/style/global.css'
 import HomePage from './pages/HomePage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HelloWorld from './pages/HelloWorld'
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path='/hello-world' element={<HelloWorld />} />
+      </Routes>
+    </Router>
   )
 }
 
