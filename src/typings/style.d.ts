79c531aa9545e04e6905444610bd38ee8de9667c
tohsaka888 <156132264@qq.com>
2022-07-29 09:06:27 +0800

    ✨ feat: ts声明module css

---
+++ src/typings/style.d.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-07-28 16:37:11
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-07-28 16:49:41
+ * @Description: 请填写简介
+ */
+
+declare module "*.module.css";
+declare module "*.css";
