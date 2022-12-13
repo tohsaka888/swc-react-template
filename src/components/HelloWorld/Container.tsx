6ccf0577d428254db7b6f9ed2bab82bbb2417a0f
tohsaka888 <156132264@qq.com>
2022-12-13 12:26:07 +0800

    ♻️ refactor: remove useless code

--- src/components/HelloWorld/Container.tsx
+++ src/components/HelloWorld/Container.tsx
@@ -1,11 +1,3 @@
-/*
- * @Author: tohsaka888
- * @Date: 2022-07-29 10:36:09
- * @LastEditors: tohsaka888
- * @LastEditTime: 2022-07-29 10:56:11
- * @Description: Hello world
- */
-
 import React from 'react'
 import { Link } from 'react-router-dom'
 import style from './index.module.css'
